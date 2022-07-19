c400dd4084411a14c37445d51827ff10a35fc39e
gleihadji <hadjigleii@gmail.com>
2022-07-19 17:06:09 +0200

    test

---
+++ Features/1_TES-2.feature
@@ -0,0 +1,7 @@
+Feature: Connexion valide 
+
+	@TEST_TES-2
+	Scenario: Connexion valide 
+		Given je suis sur la page autho  
+		When je me conecte 
+		Then je suis connecté
---
+++ src/main/java/TestRunner/TestRunner.java
@@ -0,0 +1,17 @@
+package TestRunner;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+
+@RunWith(Cucumber.class) 
+@CucumberOptions(
+features = "Features", 
+glue = { "StepDefintions" }, 
+plugin = { "pretty","json:target/cucumber-reports/Cucumber.json" }, 
+//tags = { "@TEST_TES-2" }, 
+tags="@TEST_TES-2",
+dryRun = false)
+public class TestRunner { 
+
+}
